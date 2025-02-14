016177dde721ea4f40a0c81b82cc7a056a0a93ac
Vlad Dragu <vlad.d@seomonitor.com>
2025-02-14 13:57:44 +0200

    Fix product grid text detection

--- src/Parser/Evaluated/Rule/Natural/ProductGrid.php
+++ src/Parser/Evaluated/Rule/Natural/ProductGrid.php
@@ -31,12 +31,29 @@ class ProductGrid implements ParsingRuleInterface
             $items = [];
             for ($i = 0; $i < $productGridNodes->length; $i++) {
                 if (!empty($productGridNodes->item($i))){
-                    $items[] = $productGridNodes->item($i)->getNodeValue();
+//                    $items[] = $productGridNodes->item($i)->getNodeValue();
+                    $items[] = $this->getNoteTextValue($productGridNodes->item($i));
                 }
             }
             if (count($items)) {
                 $resultSet->addItem(new BaseResult(NaturalResultType::PRODUCT_GRID, $items, $node, $this->hasSerpFeaturePosition));
             }
         }
+    }
+
+    public function getNoteTextValue($node) {
+        $text = '';
+        if (get_class($node) != 'Serps\Core\Dom\DomElement') {
+            if (get_class($node) == 'DOMText') {
+                $text = $node->nodeValue;
+            }
+        } else if ($node->getChildren()->length == 0) {
+            $text = $node->getNodeValue();
+        } else {
+            foreach ($node->getChildren() as $child) {
+                $text .= ' ' . $this->getNoteTextValue($child);
+            }
+        }
+        return trim($text);
     }
 }
