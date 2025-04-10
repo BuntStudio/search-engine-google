947a0b590b09d221d450ea33b40e2f76f094d02d
liviuseomonitor <liviu.stoicescu@seomonitor.ro>
2025-04-10 12:52:19 +0300

    #8698nqwj0 - [BUG][CRITICAL] SERP Feature news failed on 2025-04-09 - fixed identifying serp feature news

--- src/Parser/Evaluated/Rule/Natural/TopStories.php
+++ src/Parser/Evaluated/Rule/Natural/TopStories.php
@@ -13,7 +13,7 @@ class TopStories implements \Serps\SearchEngine\Google\Parser\ParsingRuleInterfa
 {
     private $hasSerpFeaturePosition = true;
     private $hasSideSerpFeaturePosition = false;
-    protected $steps = ['version1', 'version2', 'version3'];
+    protected $steps = ['version1', 'version2', 'version3', 'version4'];
 
     public function match(GoogleDom $dom, \Serps\Core\Dom\DomElement $node)
     {
@@ -146,6 +146,35 @@ class TopStories implements \Serps\SearchEngine\Google\Parser\ParsingRuleInterfa
             }
             /** @var $hrefNode DomElement */
             $items['news'][] = ['url' => \SM_Rank_Service::getUrlFromGoogleTranslate($hrefNodes->item(0)->getAttribute('href'))];
+        }
+
+        if (!empty($items)) {
+            $resultSet->addItem(new BaseResult($this->getType($isMobile), $items, $node, $this->hasSerpFeaturePosition, $this->hasSideSerpFeaturePosition));
+        }
+    }
+
+    protected function version4(GoogleDom $googleDOM, \DomElement $node, IndexedResultSet $resultSet, $isMobile)
+    {
+        $storiesIcon = $googleDOM->getXpath()->query("descendant::div[contains(@class, 'e2BEnf q8U8x')]", $node);
+        if ($storiesIcon->length == 0) {
+            return;
+        }
+
+        $hrefsNodes = $googleDOM->getXpath()->query("descendant::a[contains(@class,'WlydOe')]", $node);
+
+        if (!$hrefsNodes instanceof DomNodeList) {
+            return;
+        }
+
+        if ($hrefsNodes->length == 0) {
+            return;
+        }
+
+        $items = [];
+
+        foreach ($hrefsNodes as $hrefNode) {
+            /** @var $hrefNode DomElement */
+            $items['news'][] = ['url' => \SM_Rank_Service::getUrlFromGoogleTranslate($hrefNode->getAttribute('href'))];
         }
 
         if (!empty($items)) {
